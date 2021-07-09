6326a7e427f94b95a449ab3e235cd1b08b7792c1
Raynaldi262 <ginantaraaldi1387@gmail.com>
2021-07-10 06:08:41 +0700

    asd

--- mlp_printing/index.php
+++ mlp_printing/index.php
@@ -139,19 +139,19 @@ $data_produk = GetdataProduk($conn);
                         <div id="slider" class="carousel slide w-100" data-ride="carousel">
                             <ol class="carousel-indicators">
                                 <li data-target="#slider" data-slide-to="0" class="active"></li>
-                                <li data-target="#slider" data-slide-to="1"></li>
-                                <li data-target="#slider" data-slide-to="2"></li>
+                                <!-- <li data-target="#slider" data-slide-to="1"></li>
+                                <li data-target="#slider" data-slide-to="2"></li> -->
                             </ol>
                             <div class="carousel-inner" role="listbox">
                                 <div class="carousel-item active">
                                     <img src="images/slider-1.jpg" class="slider-img">
                                 </div>
-                                <div class="carousel-item">
+                                <!-- <div class="carousel-item">
                                     <img src="images/slider-2.jpg" class="slider-img">
                                 </div>
                                 <div class="carousel-item">
                                     <img src="images/slider-3.jpg" class="slider-img">
-                                </div>
+                                </div> -->
                             </div>
                             <a class="carousel-control-prev" href="#slider" role="button" data-slide="prev">
                                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -369,7 +369,7 @@ $data_produk = GetdataProduk($conn);
                                     </div>
                                     <div class="col-12">
                                         <address>
-                                        Pt media langit persada
+                                            Pt media langit persada
                                         </address>
                                     </div>
                                 </div>
